1c782218bc9a1ce875f90b837a146eb6fb016c16
Isaias Javier Ovelar <isaiasjovelar@gmail.com>
2026-04-15 10:55:16 -0300

    Primera version funcional

--- 3- JS/js/Data.js
+++ 3- JS/js/Data.js
@@ -2,14 +2,13 @@
 const categorias = ["Todas","Hamburguesas","Pizzas","Papas Fritas","Bebidas"];
 
 //productos
-
 const productos = [
     {
         id:1,
         nombre: "Hamburguesa Completa",
         descripcion: "Hamburguesa completa con papas",
         precio: 1200,
-        imagen:".assets/hamburguesa.jpg",
+        imagen:"./assets/hamburguesa.jpg",
         categoria:"hamburguesas"
     },
     {
@@ -17,7 +16,7 @@ const productos = [
         nombre: "Pizza Muzarella",
         descripcion: "Pizza muzzarella de 8 porciones",
         precio: 1500,
-        imagen:".assets/pizza_muzarella.jpg",
+        imagen:"./assets/pizza_muzarella.jpg",
         categoria:"Pizzas"
     },
     {
@@ -25,7 +24,7 @@ const productos = [
         nombre: "Papas Fritas",
         descripcion: "Porcion de papas fritas",
         precio: 3500,
-        imagen:".assets/papas_fritas.jpg",
+        imagen:"./assets/papas_fritas.jpg",
         categoria:"papas_fritas"
     },
     {
@@ -33,6 +32,6 @@ const productos = [
         nombre: "Coca cola",
         descripcion: "Coca Cola 500cc",
         precio: 1300,
-        imagen:".assets/coca_cola_500.jpg",
+        imagen:"./assets/coca_cola_500.jpeg",
         categoria:"bebidas"
     }];
--- 3- JS/js/main.js
+++ 3- JS/js/main.js
@@ -3,9 +3,38 @@ const listaCategorias = document.getElementById("lista-categorias");
 const cargarCategorias = () => {
     categorias.forEach(categoria => { 
         const li = document.createElement('li');
-        li.innerHTML = `<a href="#">${categoria}</a>`;  //backticks
+        li.innerHTML = `<a href="#">${categoria}</a>`;  //backticks alt 96 
         listaCategorias.appendChild(li);
     });
 }
 
 cargarCategorias();
+
+const listaProductos = document.getElementById("contenedor-productos");
+
+const cargarProductos = () => {
+    productos.forEach(producto => {
+        const article = document.createElement('article');
+        article.id = producto.id;
+        article.innerHTML = `<h3>${producto.nombre}</h3>
+                <img src="${producto.imagen}" width="250">
+                <p>${producto.descripcion}</p>
+                <p><strong>$${producto.precio}</strong></p>
+                <button type="button">Agregar</button>`;
+
+                const botonAgregar = article.querySelector("button");
+                botonAgregar.addEventListener("click", () => {
+                    //Mas adelante se agregara codigo para guardarlo en un arreglo que seria el "carrito"
+                    mensajeAnadido();
+                });
+
+        listaProductos.appendChild(article);
+    });
+}
+
+cargarProductos();
+
+//Mensaje de boton
+const mensajeAnadido = () => {
+    alert("Agregado!");
+}
