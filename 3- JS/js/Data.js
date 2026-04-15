20503c0b1cec51250e2118978169ffc3090c593c
Isaias Javier Ovelar <isaiasjovelar@gmail.com>
2026-04-15 04:41:15 -0300

    TP3 -JS

--- 3- JS/js/Data.js
+++ 3- JS/js/Data.js
@@ -1,1 +1,16 @@
-console.log("Hola Data");
+//Categorias
+const categorias = ["Todas","Hamburguesas","Pizzas","Papas Fritas","Bebidas"];
+
+//Clase producto y arreglo de productos
+class Producto{
+    constructor(nombre,imagen,precio,descripcion){
+        this.nombre=nombre;
+        this.imagen=imagen;
+        this.precio=precio;
+        this.descripcion=descripcion;
+    }
+}
+
+const listaProductos = [
+    new Producto("Hamburguesa Completa","hamburguesa.jpg",12000,"Hamburguesa completa con papas fritas y bebidas")
+]
--- 3- JS/js/main.js
+++ 3- JS/js/main.js
@@ -1,1 +1,11 @@
-console.log("Hola main");
+const listaCategorias = document.getElementById("lista-categorias");
+
+const cargarCategorias = () => {
+    categorias.forEach(categoria => { 
+        const li = document.createElement('li');
+        li.innerHTML = `<a href="#">${categoria}</a>`;  //backticks
+        listaCategorias.appendChild(li);
+    });
+}
+
+cargarCategorias();
