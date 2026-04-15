af7119fe54d3b7817d548f4abbc8f1aca909a23e
Isaias Javier Ovelar <isaiasjovelar@gmail.com>
2026-04-15 07:02:27 -0300

    .

--- 3- JS/js/Data.js
+++ 3- JS/js/Data.js
@@ -1,16 +1,38 @@
 //Categorias
 const categorias = ["Todas","Hamburguesas","Pizzas","Papas Fritas","Bebidas"];
 
-//Clase producto y arreglo de productos
-class Producto{
-    constructor(nombre,imagen,precio,descripcion){
-        this.nombre=nombre;
-        this.imagen=imagen;
-        this.precio=precio;
-        this.descripcion=descripcion;
-    }
-}
+//productos
 
-const listaProductos = [
-    new Producto("Hamburguesa Completa","hamburguesa.jpg",12000,"Hamburguesa completa con papas fritas y bebidas")
-]
+const productos = [
+    {
+        id:1,
+        nombre: "Hamburguesa Completa",
+        descripcion: "Hamburguesa completa con papas",
+        precio: 1200,
+        imagen:".assets/hamburguesa.jpg",
+        categoria:"hamburguesas"
+    },
+    {
+        id:2,
+        nombre: "Pizza Muzarella",
+        descripcion: "Pizza muzzarella de 8 porciones",
+        precio: 1500,
+        imagen:".assets/pizza_muzarella.jpg",
+        categoria:"Pizzas"
+    },
+    {
+        id:3,
+        nombre: "Papas Fritas",
+        descripcion: "Porcion de papas fritas",
+        precio: 3500,
+        imagen:".assets/papas_fritas.jpg",
+        categoria:"papas_fritas"
+    },
+    {
+        id:3,
+        nombre: "Coca cola",
+        descripcion: "Coca Cola 500cc",
+        precio: 1300,
+        imagen:".assets/coca_cola_500.jpg",
+        categoria:"bebidas"
+    }];
