485bd7ee1590d537a2f3ecb8e442453850d849ec
Isaias Javier Ovelar <isaiasjovelar@gmail.com>
2026-05-18 22:57:35 -0300

    Previo a aplicar lombok

--- 6-Lombok_DTO/TP_LOMBOK_DTO/src/main/java/com.tup.programacion3/entities/Usuario.java
+++ 6-Lombok_DTO/TP_LOMBOK_DTO/src/main/java/com.tup.programacion3/entities/Usuario.java
@@ -1,30 +1,112 @@
 package com.tup.programacion3.entities;
-
 import com.tup.programacion3.enums.Rol;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-@Getter
-@Setter
-@ToString(of = {"nombre", "apellido"})
-@EqualsAndHashCode(of = {"nombre", "apellido", "rol"})
-@NoArgsConstructor
-@AllArgsConstructor
-@SuperBuilder
 public class Usuario extends Base {
-
     private String nombre;
     private String apellido;
     private String mail;
     private String celular;
     private String contrasena;
     private Rol rol;
-    private Set<Pedido> pedidos = new HashSet<>();
+    private Set<Pedido> pedidos= new HashSet<>();
 
-    public void agregarPedido(Pedido p) {
+    public Usuario(Long id, LocalDateTime createdAt,String nombre, String apellido, String mail, String celular, String contrasena, Rol rol){
+        super(id,createdAt);
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.mail=mail;
+        this.celular=celular;
+        this.contrasena=contrasena;
+        this.rol=rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Set<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Set<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    @Override
+    public String toString(){
+        return this.nombre+","+this.apellido;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this==obj) return true;
+
+        if(obj==null || getClass()!=obj.getClass()) return false;
+
+        Usuario usuario=(Usuario)obj;
+        if(this.nombre==usuario.nombre && this.apellido==usuario.apellido && this.rol==usuario.rol) return true;
+
+        //Si no cumple ninguna condicion, por defecto es false
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(this.nombre,this.apellido);
+    }
+
+
+    //Metodo adicional, no figura en el UML
+    public void agregarPedido(Pedido p){
         pedidos.add(p);
     }
 }
