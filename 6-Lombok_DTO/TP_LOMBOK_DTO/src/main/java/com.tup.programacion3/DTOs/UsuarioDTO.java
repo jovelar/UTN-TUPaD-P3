3497bb65ca05b724b6886441813ca44b1300d5eb
Isaias Javier Ovelar <isaiasjovelar@gmail.com>
2026-05-19 08:38:19 -0300

    Funcional

---
+++ 6-Lombok_DTO/TP_LOMBOK_DTO/src/main/java/com.tup.programacion3/DTOs/UsuarioDTO.java
@@ -0,0 +1,15 @@
+package com.tup.programacion3.DTOs;
+
+import com.tup.programacion3.entities.Pedido;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record UsuarioDTO(
+        Long id,
+        LocalDateTime createdAt,
+        String nombre,
+        String apellido,
+        String mail,
+        String celular,
+        Set<Pedido> pedidos
+) {}
