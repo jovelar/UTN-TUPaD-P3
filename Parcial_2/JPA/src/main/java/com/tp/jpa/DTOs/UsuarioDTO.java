30711471382b63910a5da45ee4b7966283c13ec0
Isaias Javier Ovelar <isaiasjovelar@gmail.com>
2026-06-13 22:45:40 -0300

    Parcial 2 completo

---
+++ Parcial_2/JPA/src/main/java/com/tp/jpa/DTOs/UsuarioDTO.java
@@ -0,0 +1,16 @@
+package com.tp.jpa.DTOs;
+
+import com.tp.jpa.model.Pedido;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record UsuarioDTO(
+        //sin contraseña ni rol según lo que pedia el enunciado
+        Long id,
+        LocalDateTime createdAt,
+        String nombre,
+        String apellido,
+        String mail,
+        String celular,
+        Set<Pedido> pedidos
+) {}
